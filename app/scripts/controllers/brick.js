c5b8380d57a4e8d217344b95294af807b1224809
Jaineshkumar Patel <jenu.16@icloud.com>
2018-09-05 21:01:45 -0500

    generate angular controller brick

---
+++ app/scripts/controllers/brick.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name controllerQuizApp.controller:BrickCtrl
+ * @description
+ * # BrickCtrl
+ * Controller of the controllerQuizApp
+ */
+angular.module('controllerQuizApp')
+  .controller('BrickCtrl', function () {
+
+  });
