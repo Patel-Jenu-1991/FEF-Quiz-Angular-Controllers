e54ede2819eda8c2e4f897bcdf7cc51b2db15529
Jaineshkumar Patel <jenu.16@icloud.com>
2018-09-06 20:07:30 -0500

    Add controller properties, update view

--- app/scripts/controllers/brick.js
+++ app/scripts/controllers/brick.js
@@ -9,5 +9,8 @@
  */
 angular.module('controllerQuizApp')
   .controller('BrickCtrl', function () {
-
+    this.color = 'Green';
+    this.size = '2 x 6';
+    this.price = 0.03;
+    this.quantity = 697;
   });
